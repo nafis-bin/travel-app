d6fb00b273c1a69ca5c462f46b976df6e0f235a6
nafis bin <nafisbinnizam@gmail.com>
2025-09-16 23:51:31 +0600

    assets: added new assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="bg-amber-300">Hello world</h1>
+    <h1 className="text-2xl font-bold text-teal-500">
+      Hello world
+    </h1>
   );
 }
